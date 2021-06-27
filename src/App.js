1b1edea3eba31b171a488f03a7506124b6d1f812
youkey1001 <youkey_1001@yahoo.co.jp>
2021-06-27 14:19:45 +0900

    create my own React boilerplate

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App-title">Hello, world!</h1>
-      </header>
-    </div>
-  );
+const App = () => {
+  return <div><h1>Hello, world!</h1></div>;
 }
 
 export default App;
