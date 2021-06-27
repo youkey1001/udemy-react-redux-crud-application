e76e27863d124e278a9d7364d2371dcf0f2bb3cf
youkey1001 <youkey_1001@yahoo.co.jp>
2021-06-27 21:43:19 +0900

    fix typo for actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,10 +2,10 @@ export const INCREMENT = 'INCREMENT';
 export const DECREMENT = 'DECREMENT';
 
 
-export const increment = () => {{
+export const increment = () => ({
     type: INCREMENT
-}}
+})
 
-export const decrement = () => {{
+export const decrement = () => ({
     type: DECREMENT
-}} 
+})
